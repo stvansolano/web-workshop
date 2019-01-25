2a6b709c78e1e29b9010abce3b56ce6bb5978900
stvansolano <stvansolano@outlook.com>
2019-01-25 12:39:27 -0600

    Challenge 6: XMLHttpRequest what?

--- webservices.js
+++ webservices.js
@@ -3,5 +3,10 @@ const URL = "https://web-students-1aa35.firebaseio.com/group-01.json";
 fetchData();
 
 function fetchData(){
-    
+ 
+  var request = new XMLHttpRequest();
+  request.open('GET', URL, true);
+
+  request.send();
 }
+
